6e58ac555114391e408d577d21dd801810d23a10
mythking-git <70135452+mythking-git@users.noreply.github.com>
2023-06-25 23:44:25 +0100

    Update word iterations.py

--- word iterations.py
+++ word iterations.py
@@ -1,5 +1,7 @@
 from collections import Counter, defaultdict
-import itertools
+import math
+import numpy as np
+
 
 """
 def divideList(lst):
@@ -14,14 +16,15 @@ def divideList(lst):
     return res
 """
 
+
 r = input("length required ")
-q = input("larger than  ")
+q = input("at least: ")
 
-f = open("allscrablewords.txt","r")
-#f = open("words_alpha.txt", "r")
+#f = open("allscrablewords.txt","r")
+f = open("words_alpha.txt", "r")
 
-# f"best {r} letter words.txt"
-name = f"words alpha {r} letter words.txt"
+name = f"best {r} letter words.txt"
+#name = f"scrabble {r} letter words.txt"
 
 st = []
 ranked=[]
@@ -29,8 +32,15 @@ control=[]
 
 for readline in f: 
     line_strip = readline.strip()
-    st.append(''.join(sorted(line_strip)))
     control.append(line_strip)
+    temp = ''.join(sorted(line_strip))
+    st.append(temp)
+
+
+pairedcontrol = []
+
+for letters in range(len(control)):
+    pairedcontrol.append(''.join(sorted(control[letters])))
 
 #f = open("orderedscrable.txt", "w")
 #f.write(str(st))
@@ -61,7 +71,6 @@ for readline in f:
 #f = open("rankedscrable.txt", "w")
 #f.write(str(counts))
 
-
 winners = st
 counts = Counter(winners)
 
@@ -74,10 +83,33 @@ sorted(ranked,key=lambda l:l[0], reverse=True)
 
 sorted = sorted(ranked,key=lambda l:l[0] , reverse=True)
 
+words=[]
+bestcombo = []
+
+finalwords = {}
+
 f = open(name, "w")
+"""
+for sorts in range(len(sorted)):
+    if len(sorted[sorts][1]) == int(r):
+        bestcombo.append(sorted[sorts][0])"""
+
+fact = int(math.factorial(int(r)))
+
+#max = int(max(bestcombo))
+max = 1
+
+f.write(f"{max} out of {fact} possible letter combinations - {np.format_float_positional(((max / fact)*100), trim='-')}%\n\n" )
+
 for x in range(len(sorted)):
-    if int(r) == len(sorted[x][1]) and int(q) < sorted[x][0]:
-        tempstring = str(sorted[x][0]) + " - " + str(sorted[x][1]) + "\n"
+    if int(r) == len(sorted[x][1]) and sorted[x][0] >= int(q):
+        
+        for items in range(len(control)):
+            if pairedcontrol[items] == sorted[x][1]:
+                finalwords.setdefault(sorted[x][1], []).append(control[items])
+                #print(finalwords)
+
+        tempstring = str(sorted[x][0]) + " - " + str(sorted[x][1]) + " - " + str(finalwords[sorted[x][1]]) + "\n"
         f.write(tempstring)
 
 #f = open("rankedscrable.txt", "w")
